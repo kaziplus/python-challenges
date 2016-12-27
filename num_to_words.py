7bf646a7054e97fab39fcc38c8685af35e3e9eb9
Anthony Nandaa <profnandaa@gmail.com>
2016-12-27 23:21:21 +0300

    initial structure

---
+++ num_to_words.py
@@ -0,0 +1,11 @@
+'''
+Write a function `num_to_words(N)` that takes in a number, N,
+and converts each of the digits that make up that number in to 
+the word equivalent.
+
+For example:
+    - `num_to_words(4500)` returns `"four five zero zero"`
+'''
+
+def num_to_words(N):
+    pass
